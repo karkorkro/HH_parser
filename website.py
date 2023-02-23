b1417099f57e0922538c5349a3a450d3a5d81d71
karkorkro <kara_study@mail.ru>
2023-02-23 17:00:07 +0300

    css templates moved to static folder, html files moved to templates folder, website file created with all flask website logic.

--- website.py
+++ website.py
@@ -4,29 +4,27 @@ from vacancies import skill_search
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.get('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/contacts/')
+@app.get('/contacts/')
 def contacts():
     return render_template('contacts.html')
 
 
-@app.route('/search/', methods=['GET', 'POST'])
+@app.post('/search/')
 def search():
-    if request.method == 'POST':
-        subject = request.form['search_subject']
-        skills = skill_search(subject)[::-1]
-        return render_template('results.html', skills=skills, subject=subject)
-    else:
-        return render_template('search.html')
+
+    subject = request.form['search_subject']
+    skills = skill_search(subject)[::-1]
+    return render_template('results.html', skills=skills, subject=subject)
 
 
-@app.route('/results/')
-def results():
-    return render_template('vacancies_info.json')
+@app.get('/search/')
+def show_search_page():
+    return render_template('search.html')
 
 
 if __name__ == '__main__':
